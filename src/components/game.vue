d1da56d0ffe18759ee863cc2e47f2c8e3bae7109
deniskozlov <denisyurievich94@gmail.com>
2018-12-10 18:18:39 +0300

    Scatter integration. Bet size calculation changes

--- src/components/game.vue
+++ src/components/game.vue
@@ -1,261 +1,272 @@
 <template>
-    <section class="section">
-        <div class="container">
-            <div class="box">
-                <div class="columns">
-                    <div class="column is-two-thirds">
-                        <label class="label is-small">BET AMOUNT</label>
-                        <div class="field has-addons">
-                            <div class="control is-expanded has-icon-right">
-                                <input class="input" type="text" v-model="bet" />
-                            </div>
+  <section class="section">
+    <div class="container">
+      <div class="box">
+        <div class="columns">
+          <div class="column is-two-thirds">
+            <label class="label is-small">BET AMOUNT</label>
+            <div class="field has-addons">
+              <div class="control is-expanded has-icon-right">
+                <input class="input" type="text" v-model="bet">
+              </div>
 
-                            <div class="control">
-                                <button class="button" @click="setBet(0.5)">
-                                    1/2
-                                </button>
-                            </div>
-                            <div class="control">
-                                <button class="button" @click="setBet(2)">
-                                    2X
-                                </button>
-                            </div>
-                            <div class="control">
-                                <button class="button" @click="setBet()">
-                                    MAX
-                                </button>
-                            </div>
-                        </div>
-                    </div>
-                    <div class="column">
-                        <label class="label is-small">PAYOUT ON WIN</label>
-                        <div class="field has-addons">
-                            <div class="control is-expanded">
-                                <input class="input" type="text" v-model="payWin">
-                            </div>
-                        </div>
-                    </div>
-                </div>
-                <div class="box">
-                    <div class="columns is-mobile">
-                        <div class="column has-text-centered">
-                            <label class="label is-small">ROLL UNDER TO WIN</label>
-                            <h3 class="title is-3">{{rollUnder}}↓</h3>
-                        </div>
-                        <div class="column has-text-centered">
-                            <label class="label is-small">PAYOUT</label>
-                            <h3 class="title is-3">{{Number(payOut).toFixed(2)}}x</h3>
-                        </div>
-                        <div class="column has-text-centered">
-                            <label class="label is-small">WIN CHANCE</label>
-                            <h3 class="title is-3">{{winChance}}%</h3>
-                        </div>
-                    </div>
-                </div>
-                <div class="columns">
-                    <div class="column has-text-centered">
-                        <div class="field is-expanded">
-                            <vue-slider ref="slider" v-model="rollUnder" v-bind="sliderOptions"></vue-slider>
-                        </div>
-                    </div>
-                </div>
-                <div class="columns is-mobile is-vcentered">
-                    <div class="column has-text-centered">
-                        <span class="title is-5">{{Number(currentEOS).toFixed(4)}} EOS</span>
-                    </div>
-                    <div class="column has-text-centered">
-                        <button v-if="account.name" class="button">ROLL DICE</button>
-                        <button v-else @click="login" class="button">LOGIN</button>
-                    </div>
-                    <div class="column has-text-centered">
-                        <span class="title is-5"></span>
-                    </div>
-                </div>
+              <div class="control">
+                <button class="button" @click="setBet(0.5)">1/2</button>
+              </div>
+              <div class="control">
+                <button class="button" @click="setBet(2)">2X</button>
+              </div>
+              <div class="control">
+                <button class="button" @click="setBet()">MAX</button>
+              </div>
             </div>
+          </div>
+          <div class="column">
+            <label class="label is-small">PAYOUT ON WIN</label>
+            <div class="field has-addons">
+              <div class="control is-expanded">
+                <input class="input" type="text" v-model="payWin">
+              </div>
+            </div>
+          </div>
         </div>
-    </section>
+        <div class="box">
+          <div class="columns is-mobile">
+            <div class="column has-text-centered">
+              <label class="label is-small">ROLL UNDER TO WIN</label>
+              <h3 class="title is-3">{{rollUnder}}↓</h3>
+            </div>
+            <div class="column has-text-centered">
+              <label class="label is-small">PAYOUT</label>
+              <h3 class="title is-3">{{Number(payOut).toFixed(2)}}x</h3>
+            </div>
+            <div class="column has-text-centered">
+              <label class="label is-small">WIN CHANCE</label>
+              <h3 class="title is-3">{{winChance}}%</h3>
+            </div>
+          </div>
+        </div>
+        <div class="columns">
+          <div class="column has-text-centered">
+            <div class="field is-expanded">
+              <vue-slider ref="slider" v-model="rollUnder" v-bind="sliderOptions"></vue-slider>
+            </div>
+          </div>
+        </div>
+        <div class="columns is-mobile is-vcentered">
+          <div class="column has-text-centered">
+            <span class="title is-5">{{Number(currentEOS).toFixed(4)}} EOS</span>
+          </div>
+          <div class="column has-text-centered">
+            <button v-if="account.name" class="button">ROLL DICE</button>
+            <button v-else @click="login" class="button">LOGIN</button>
+          </div>
+          <div class="column has-text-centered">
+            <span class="title is-5"></span>
+          </div>
+        </div>
+      </div>
+    </div>
+  </section>
 </template>
 
 <script>
-    import vueSlider from 'vue-slider-component'
-    import {network} from '../network'
-    import Eos from 'eosjs'
-    import ScatterJS from 'scatterjs-core'
-    import ScatterEOS from 'scatterjs-plugin-eosjs2'
+import vueSlider from "vue-slider-component";
+import { network } from "../network.js";
+import { Api, JsonRpc, JsSignatureProvider } from "eosjs";
+import ScatterJS from "scatterjs-core";
+import ScatterEOS from "scatterjs-plugin-eosjs2";
 
-    ScatterJS.plugins(new ScatterEOS())
+//ScatterJS.plugins(new ScatterEOS());
 
-    const endpoint = network.protocol + '://' + network.host + ':' + network.port
-    const scatter = ScatterJS.scatter
-    //const eos = scatter.eos(network, Eos, { expireInSeconds:60, debug: true })
-    const eos = Eos({httpEndpoint:endpoint, signatureProvider:ScatterJS.scatter.eosHook(network)})
+const endpoint = network.protocol + "://" + network.host + ":" + network.port;
+const rpc = new JsonRpc(endpoint);
 
+export default {
+  components: {
+    vueSlider
+  },
+  mounted() {
+    this.getBalance();
+    console.log(network);
+    this.getPool();
+  },
+  data() {
+    return {
+      rollUnder: 50,
+      bet: 1,
+      currentEOS: 0,
+      availableBalance: 0,
+      sliderOptions: {
+        data: null,
+        eventType: "auto",
+        width: "auto",
+        height: 10,
+        dotSize: 20,
+        dotHeight: null,
+        dotWidth: null,
+        min: 2,
+        max: 96,
+        interval: 1,
+        show: true,
+        speed: 0.1,
+        disabled: false,
+        piecewise: false,
+        usdKeyboard: false,
+        enableCross: true,
+        piecewiseStyle: false,
+        piecewiseLabel: false,
+        tooltip: "always",
+        tooltipDir: "top",
+        reverse: false,
+        data: null,
+        clickable: true,
+        realTime: false,
+        lazy: false,
+        formatter: null,
+        bgStyle: null,
+        sliderStyle: null,
+        processStyle: null,
+        piecewiseActiveStyle: null,
+        piecewiseStyle: null,
+        tooltipStyle: null,
+        labelStyle: null,
+        labelActiveStyle: null
+      }
+    };
+  },
+  methods: {
+    getBalance() {
+      if (!this.account.name) {
+        this.currentEOS = 0;
+        return;
+      }
+      rpc
+        .get_table_rows({
+          code: "eosio.token",
+          scope: this.account.name,
+          table: "accounts"
+        })
+        .then(accountBalance => {
+          if (accountBalance.rows.length) {
+            this.currentEOS = accountBalance.rows[0].balance.slice(0, -4);
+          }
+        });
+    },
+    getPool() {
+      Promise.all([
+        rpc.get_table_rows({
+          code: "eosio.token",
+          scope: "sevenscasino",
+          table: "accounts"
+        }),
+        rpc.get_table_rows({
+          code: "sevenscasino",
+          scope: "sevenscasino",
+          table: "tabenvs"
+        })
+      ]).then(([accountBalance, locked]) => {
+        this.availableBalance =
+          accountBalance.rows[0].balance.slice(0, -4) -
+          locked.rows[0].locked.slice(0, -4);
+      });
+    },
+    setBet(rate) {
+      let minBet = 0.1;
+      const { availableBalance, currentEOS } = this;
+      let bet = rate ? this.bet * rate : this.maxBetAmount();
 
-    export default {
-        components: {
-            vueSlider
-        },
-        mounted() {
-            this.getBalance()
-            console.log(network)
-            //this.getPool()
-        },
-        data() {
-            return {
-                rollUnder: 50,
-                bet: 1,
-                currentEOS: 0,
-                poolBalance: 99,
-                sliderOptions: {
-                    data: null,
-                    eventType: 'auto',
-                    width: 'auto',
-                    height: 10,
-                    dotSize: 20,
-                    dotHeight: null,
-                    dotWidth: null,
-                    min: 2,
-                    max: 96,
-                    interval: 1,
-                    show: true,
-                    speed: 0.1,
-                    disabled: false,
-                    piecewise: false,
-                    usdKeyboard: false,
-                    enableCross: true,
-                    piecewiseStyle: false,
-                    piecewiseLabel: false,
-                    tooltip: 'always',
-                    tooltipDir: 'top',
-                    reverse: false,
-                    data: null,
-                    clickable: true,
-                    realTime: false,
-                    lazy: false,
-                    formatter: null,
-                    bgStyle: null,
-                    sliderStyle: null,
-                    processStyle: null,
-                    piecewiseActiveStyle: null,
-                    piecewiseStyle: null,
-                    tooltipStyle: null,
-                    labelStyle: null,
-                    labelActiveStyle: null
-                }
-            }
-        },
-        methods: {
-            getBalance() {
-                if (!this.account) {
-                    this.currentEOS = 0;
-                    return;
-                }
-                /*return api.getAccount(this.account.name).then(({
-                    core_liquid_balance
-                }) => {
-                    this.currentEOS = Number(core_liquid_balance.replace(/\sEOS/, ''))
-                })*/
-                return 100  // MOCK
-            },
-            getPool() {
-                Promise.all([
-                    api.getTableRows({
-                        json: true,
-                        code: 'eosio.token',
-                        table: 'accounts',
-                        scope: this.$contractAccount
-                    }),
-                    api.getTableRows({
-                        json: true,
-                        code: this.$contractAccount,
-                        table: 'envs',
-                        scope: this.$contractAccount
-                    })
-                ]).then(([accountBalance, poolBalance]) => {
-                    this.poolBalance = accountBalance.rows[0].balance.slice(0, -4) -
-                        poolBalance.rows[0].locked.slice(0, -4)
-                });
-            },
-            setBet(rate) {
-                const {
-                    poolBalance,
-                    currentEOS
-                } = this
-                let bet = rate ? this.bet * rate : this.currentEOS
-                console.log("BET: " + bet, "max bet amount: " + this.maxBetAmount(), " current eos: ", currentEOS)
-                switch (true) {
-                    case (bet < 0.1):
-                        bet = 0.1
-                        break
-                    case (bet > currentEOS):
-                        bet = currentEOS
-                        break
-                    case (bet > this.maxBetAmount()):
-                        bet = this.maxBetAmount()
-                        break
-                }
-                console.log("Bet: ", bet)
-                this.bet = Number(bet).toFixed(4)
-            },
+      console.log(
+        "BET: " + bet,
+        "max bet amount: " + this.maxBetAmount(),
+        " current eos: ",
+        currentEOS
+      );
 
-            floor(value, decimals) {
-                return Number(Math.floor(value + 'e' + decimals) + 'e-' + decimals)
-            },
+      if (bet < minBet) {
+        bet = minBet;
+      }
+      if (bet > currentEOS && currentEOS > minBet) {
+        bet = currentEOS;
+      }
+      if (bet > this.maxBetAmount() && this.maxBetAmount() > minBet) {
+        bet = this.maxBetAmount();
+      }
 
-            maxBetAmount() {
-                return this.floor(this.poolBalance / 5 / (98 / this.winChance), 4)
-            },
+      console.log("Bet: ", bet);
+      this.bet = Number(bet).toFixed(4);
+    },
 
-            login() {
-                console.log(this.$contractAccount)
-                const requiredFields = {
-                    accounts: [network]
-                }
+    floor(value, decimals) {
+      return Number(Math.floor(value + "e" + decimals) + "e-" + decimals);
+    },
 
-                ScatterJS.scatter.connect(this.$contractAccount).then(connected => {
-                    scatter.suggestNetwork(network).then(added => {
-                        console.log('Network suggest ok')
-                        scatter.getIdentity(requiredFields).then(() => {
-                            console.log('Got identity success')
-                            const account = scatter.identity.accounts.find(x => x.blockchain ===
-                                'eos')
-                            console.log("this account: " + account.name)
-                            this.$store.commit('UPDATE_ACCOUNT', account)
-                        }).catch(e => {
-                            console.log('User rejected select identity')
-                            console.log(e.message)
-                        })
-                        window.ScatterJS = null
-                    }).catch(e => {
-                        console.log(e.message)
-                    })
-                }).catch(e => {
-                    console.log('Scatter Connecting issue')
-                    console.log(e.message)
-                    // Sent event to open Download scatter modal is here
+    maxBetAmount() {
+      return this.floor(this.availableBalance / 5 / (98 / this.winChance), 4);
+    },
+
+    login() {
+      const scatter = ScatterJS.scatter;
+      const requiredFields = {
+        accounts: [network]
+      };
+
+      scatter
+        .connect(this.$contractAccount)
+        .then(connected => {
+          scatter
+            .suggestNetwork(network)
+            .then(added => {
+              console.log("Network suggest ok");
+              scatter
+                .getIdentity(requiredFields)
+                .then(() => {
+                  console.log("Got identity success");
+                  const account = scatter.identity.accounts.find(
+                    x => x.blockchain === "eos"
+                  );
+                  console.log("this account: " + account.name);
+                  this.$store.commit("UPDATE_ACCOUNT", account);
                 })
-            },
-        },
-        computed: {
-            winChance() {
-                return this.rollUnder - 1
-            },
-
-            payOut() {
-                return 98 / this.winChance
-            },
-
-            payWin() {
-                return (this.bet * this.payOut).toFixed(4)
-            },
-            account() {
-                return this.$store.state.account
-            }
-        },
-        watch: {
-            account(){
-                console.log("Hi from {account} property watcher")
-            }
-        }
+                .catch(e => {
+                  console.log("User rejected select identity");
+                  console.log(e.message);
+                });
+              window.ScatterJS = null;
+            })
+            .catch(e => {
+              console.log(e.message);
+            });
+        })
+        .catch(e => {
+          console.log("Scatter Connecting issue");
+          console.log(e.message);
+          // Sent event to open Download scatter modal is here
+        });
     }
+  },
+  computed: {
+    winChance() {
+      return this.rollUnder - 1;
+    },
+
+    payOut() {
+      return 98 / this.winChance;
+    },
+
+    payWin() {
+      return (this.bet * this.payOut).toFixed(4);
+    },
+
+    account() {
+      return this.$store.state.account;
+    }
+  },
+  watch: {
+    account() {
+      console.log("Hi from {account} property watcher");
+      this.getBalance();
+    }
+  }
+};
 </script>
--- src/components/header.vue
+++ src/components/header.vue
@@ -1,56 +1,98 @@
 <template>
-    <header class="header">
-        <nav class="navbar is-primary">
-            <div class="container">
-                <div class="navbar-brand">
-                    <a class="navbar-item" href="#" style="font-weight:bold;">EOS777</a>
-                    <span class="navbar-burger burger" @click="showNav = !showNav" :class="{ 'is-active': showNav }">
-                        <span></span>
-                        <span></span>
-                        <span></span>
-                    </span>
-                </div>
-                <div class="navbar-menu" :class="{ 'is-active': showNav }">
-                    <div class="navbar-end">
-                        <a href="#" class="navbar-item">Affiliate</a>
-                        <a href="#" class="navbar-item">Tokens Airdrop</a>
-                        <a href="#" class="navbar-item">How to play</a>
-                        <span class="navbar-item">
-                            <a v-if="account.name" @click="logout" class="button is-link">Logout {{account.name}}</a>
-                            <a v-else @click="login" class="button is-link">Login</a>
-                        </span>
-                    </div>
-                </div>
-            </div>
-        </nav>
-    </header>
+  <header class="header">
+    <nav class="navbar is-primary">
+      <div class="container">
+        <div class="navbar-brand">
+          <a class="navbar-item" href="#" style="font-weight:bold;">EOS777</a>
+          <span
+            class="navbar-burger burger"
+            @click="showNav = !showNav"
+            :class="{ 'is-active': showNav }"
+          >
+            <span></span>
+            <span></span>
+            <span></span>
+          </span>
+        </div>
+        <div class="navbar-menu" :class="{ 'is-active': showNav }">
+          <div class="navbar-end">
+            <a href="#" class="navbar-item">Affiliate</a>
+            <a href="#" class="navbar-item">Tokens Airdrop</a>
+            <a href="#" class="navbar-item">How to play</a>
+            <span class="navbar-item">
+              <a v-if="account.name" @click="logout" class="button is-link">Logout {{account.name}}</a>
+              <a v-else @click="login" class="button is-link">Login</a>
+            </span>
+          </div>
+        </div>
+      </div>
+    </nav>
+  </header>
 </template>
 
-
 <script>
-    export default {
-        data() {
-            return {
-                showNav: false
-            }
-        },
-        methods: {
-            login() {
-                console.log("Login from header, no scatter object here")
-                // need to use scatter here
-            },
-            logout() {
-                //this.$scatter.forgetIdentity().then(() => {
-                //    this.$message.success('User logout success')
-                console.log("Logout from header, no scatter object here")
-                this.$store.commit('UPDATE_ACCOUNT', {})
-                //})
-            }
-        },
-        computed: {
-            account() {
-                return this.$store.state.account
-            }
-        }
+import ScatterJS from "scatterjs-core";
+import { network } from "../network.js";
+
+const scatter = ScatterJS.scatter;
+
+export default {
+  data() {
+    return {
+      showNav: false
+    };
+  },
+  methods: {
+    login() {
+      console.log("Login from header");
+      const requiredFields = {
+        accounts: [network]
+      };
+
+      scatter
+        .connect(this.$contractAccount)
+        .then(connected => {
+          scatter
+            .suggestNetwork(network)
+            .then(added => {
+              console.log("Network suggest ok");
+              scatter
+                .getIdentity(requiredFields)
+                .then(() => {
+                  console.log("Got identity success");
+                  const account = scatter.identity.accounts.find(
+                    x => x.blockchain === "eos"
+                  );
+                  console.log("this account: " + account.name);
+                  this.$store.commit("UPDATE_ACCOUNT", account);
+                })
+                .catch(e => {
+                  console.log("User rejected select identity");
+                  console.log(e.message);
+                });
+              window.ScatterJS = null;
+            })
+            .catch(e => {
+              console.log(e.message);
+            });
+        })
+        .catch(e => {
+          console.log("Scatter Connecting issue");
+          console.log(e.message);
+          // Sent event to open Download scatter modal is here
+        });
+    },
+    logout() {
+      scatter.forgetIdentity().then(() => {
+        console.log("User logout success from header");
+        this.$store.commit("UPDATE_ACCOUNT", {});
+      });
     }
+  },
+  computed: {
+    account() {
+      return this.$store.state.account;
+    }
+  }
+};
 </script>
